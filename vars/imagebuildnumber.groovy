e40ff166a9910fefb4a4b6b388febe20990aadda
Sumit Kumari <jasbirsumit@gmail.com>
2022-12-13 17:07:28 +0530

    image name changes

--- vars/imagebuildnumber.groovy
+++ vars/imagebuildnumber.groovy
@@ -1,5 +1,5 @@
 import java.text.SimpleDateFormat
 
 def call() {
-    currentBuild.displayName = ${app_name}-new Date().format("yyyyMMdd'T'HHmm") + "-" + env.BUILD_NUMBER
+    currentBuild.displayName = ${"app_name"}-new Date().format("yyyyMMdd'T'HHmm") + "-" + env.BUILD_NUMBER
 }
