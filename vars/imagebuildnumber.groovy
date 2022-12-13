980b6f30fdf53716cd8ca336596cd70bc69ad596
Sumit Kumari <jasbirsumit@gmail.com>
2022-12-13 18:31:56 +0530

    adding variable

--- vars/imagebuildnumber.groovy
+++ vars/imagebuildnumber.groovy
@@ -1,7 +1,7 @@
 import java.text.SimpleDateFormat
 
-def call() {
+def call(app_name) {
     //currentBuild.displayName = new SimpleDateFormat("yy.MM.dd").format(new Date()) + "-" + env.BUILD_NUMBER
-    currentBuild.displayName = new Date().format("yyyyMMdd'T'HHmm")+ "-" + env.BUILD_NUMBER
-    
+    //currentBuild.displayName = new Date().format("yyyyMMdd'T'HHmm")+ "-" + env.BUILD_NUMBER
+    currentBuild.displayName = ${app_name}
 }
