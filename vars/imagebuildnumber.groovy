f68da22b7694c78f069c33ec8e100136c674fa1b
Sumit Kumari <jasbirsumit@gmail.com>
2022-12-20 16:23:29 +0530

    build nu

--- vars/imagebuildnumber.groovy
+++ vars/imagebuildnumber.groovy
@@ -2,5 +2,6 @@ import java.text.SimpleDateFormat
 
 def call() {
     //currentBuild.displayName = new SimpleDateFormat("yy.MM.dd").format(new Date()) + "-" + env.BUILD_NUMBER
-    currentBuild.displayName = new Date().format("yyyyMMdd'T'HHmm")+ "-" + env.BUILD_NUMBER
+    //currentBuild.displayName = new Date().format("yyyyMMdd'T'HHmm")+ "-" + env.BUILD_NUMBER
+    currentBuild.displayName = "test-${build_number}"
 }
