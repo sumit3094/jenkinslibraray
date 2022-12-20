adae505aa881f08ee49522715f831e3cc0e3492c
Sumit Kumari <jasbirsumit@gmail.com>
2022-12-20 11:37:07 +0530

    adding script

--- vars/getscm.groovy
+++ vars/getscm.groovy
@@ -1,1 +1,4 @@
-def repoUrl = checkout(scm).GIT_URL
+//def repoUrl = checkout(scm).GIT_URL
+String determineRepoName() {
+    return scm.getUserRemoteConfigs()[0].getUrl().tokenize('/')[3].split("\\.")[0]
+}
