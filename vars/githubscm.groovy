a147f7ddfb7b51a73384c700ee15400037ff27cd
Sumit Kumari <jasbirsumit@gmail.com>
2022-12-20 16:30:31 +0530

    file changes

--- vars/githubscm.groovy
+++ vars/githubscm.groovy
@@ -1,25 +1,11 @@
 //https://github.com/aleksei-bulgak/jenkins-shared-library-example/blob/master/src/by/bulgak/jenkins/lib/Checkout.groovy
+//https://devopscube.com/create-jenkins-shared-library/
 
-def checkoutIfExists(String repository, String branch) {
-//def checkoutIfExists(String repository, String branches, def credentials = ['token': 'abc-token', 'usernamePassword': 'abc']) {
-   // assert credentials['token']
-    //assert credentials['usernamePassword']
-  //  def sourceAuthor = author
-    def sourceRepository = getForkedProjectName(defaultAuthor, repository, sourceAuthor, credentials['token']) ?: repository
-    // Checks source group and branch (for cases where the branch has been created in the author's forked project)
-    def repositoryScm = getRepositoryScm(sourceRepository, author, branches, credentials['usernamePassword'])
-    if (repositoryScm == null) {
-        // Checks target group and and source branch (for cases where the branch has been created in the target project itself
-        repositoryScm = getRepositoryScm(repository, defaultAuthor, branches, credentials['usernamePassword'])
-        sourceAuthor = repositoryScm ? defaultAuthor : author
-    }
-    if (repositoryScm != null && (!mergeTarget || hasPullRequest(defaultAuthor, repository, author, branches, credentials['token']))) {
-        if (mergeTarget) {
-            mergeSourceIntoTarget(sourceRepository, sourceAuthor, branches, repository, defaultAuthor, defaultBranches, credentials['usernamePassword'])
-        } else {
-            checkout repositoryScm
-        }
-    } else {
-        checkout(resolveRepository(repository, defaultAuthor, defaultBranches, false, credentials['usernamePassword']))
-    }
-}
+def call(Map stageParams) {
+
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
+  }
--- vars/imagebuildnumber.groovy
+++ vars/imagebuildnumber.groovy
@@ -5,3 +5,5 @@ def call() {
     //currentBuild.displayName = new Date().format("yyyyMMdd'T'HHmm")+ "-" + env.BUILD_NUMBER
     currentBuild.displayName = "test-${build_number}"
 }
+
+//https://github.com/liejuntao001/jenkins-buildnumber-lib/blob/master/testjobs/buildnumber_Jenkinsfile.groovy
